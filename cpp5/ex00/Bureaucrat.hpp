3cb5a72be0158f121e8b67493ad64553291dda54
pablomon <pablomonteserin@gmail.com>
2022-02-01 19:07:34 +0100

    cpp5 ex00 done

---
+++ cpp5/ex00/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class Bureaucrat {
+	private:
+	const std::string name;
+	int grade;
+
+	public:
+	const std::string getName() const;
+	int getGrade() const;
+
+	class GradeTooHighException : public std::exception {
+		public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+		public:
+		const char *what() const throw();
+	};
+
+	void gradeUp();
+	void gradeDown();
+
+	Bureaucrat(int grade);
+	Bureaucrat();
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat &bureaucrat);
+	Bureaucrat &operator=(const Bureaucrat &bureaucrat);
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ cpp5/ex00/main.cpp
@@ -0,0 +1,39 @@
+#include "Bureaucrat.hpp"
+
+int main(void) 
+{
+	std::cout << "Creating a default bureaucrat\n";
+	Bureaucrat *b1 = new Bureaucrat();
+	std::cout << "Current bureaucrat grade " << b1->getGrade() << " \n";
+	std::cout << "Trying to decrease grade\n";
+	for (size_t i = 0; i < 3; i++)
+	{
+		try {
+			b1->gradeDown();
+		}
+		catch(const std::exception& e)	{
+			std::cout << e.what() << '\n';
+		}
+	}
+	
+	std::cout << "Current bureaucrat grade " << b1->getGrade() << " \n";
+	b1->gradeUp();
+	b1->gradeUp();
+	std::cout << "Current bureaucrat grade " << b1->getGrade() << " \n";
+	delete b1;
+
+	std::cout << "\nCreating a top notch bureaucrat\n";
+	b1 = new Bureaucrat(2);
+	std::cout << "Current bureaucrat grade " << b1->getGrade() << " \n";
+	for (size_t i = 0; i < 3; i++)
+	{
+		try {
+			b1->gradeUp();
+		}
+		catch(const std::exception& e)	{
+			std::cout << e.what() << '\n';
+		}
+	}
+	std::cout << "Current bureaucrat grade " << b1->getGrade() << " \n";
+	return 0;
+}
