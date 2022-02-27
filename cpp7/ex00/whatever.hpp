bd40c5907558e7c63b1c44550f0a0b2a4db56730
pablomon <pablomonteserin@gmail.com>
2022-02-27 21:42:27 +0100

    cpp7 exp00 done

---
+++ cpp7/ex00/whatever.hpp
@@ -0,0 +1,32 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <typeinfo>
+
+template <typename T>
+void swap(T &item1, T &item2)
+{
+	T tmp;
+
+	tmp = item1;
+	item1 = item2;
+	item2 = tmp;
+};
+
+template <typename T>
+T min(T &item1, T &item2)
+{
+	if (item1 >= item2)
+		return item2;
+	return item1;
+};
+
+template <typename T>
+T max(T item1, T item2)
+{
+	if (item1 <= item2)
+		return item2;
+	return item1;
+};
+
+#endif
