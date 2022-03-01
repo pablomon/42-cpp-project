dec14bcb78c8b655359c5144ae9097fa1e5118ef
pablomon <pablomonteserin@gmail.com>
2022-03-02 00:18:31 +0100

    cpp1 improved ex05

--- cpp1/ex05/main.cpp
+++ cpp1/ex05/main.cpp
@@ -3,22 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmontese <pmontes@student.42madrid.com>    +#+  +:+       +#+        */
+/*   By: pmontese <pmontese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 10:55:58 by pmontese          #+#    #+#             */
-/*   Updated: 2022/01/08 15:49:53 by pmontese         ###   ########.fr       */
+/*   Updated: 2022/03/02 00:17:59 by pmontese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "karen.hpp"
+#include <iostream>
 
 int main(void)
 {
 	Karen *k = new Karen();
+	std::cout << "DEBUG:" << std::endl;
 	k->complain("debug");
+	std::cout << "INFO:" << std::endl;
 	k->complain("info");
+	std::cout << "WARNING:" << std::endl;
 	k->complain("warning");
+	std::cout << "ERROR:" << std::endl;
 	k->complain("error");
+	std::cout << "INVALID:" << std::endl;
 	k->complain("invalid option defaults to debug");
 	delete k;
 	return 0;
