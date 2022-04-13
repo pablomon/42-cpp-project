4c3cce03856edeefba693d2fefea5804ea422fb3
Pablomon <pablomonteserin@gmail.com>
2022-04-13 13:21:23 +0200

    cleaned cpp3

--- cpp3/ex03/main.cpp
+++ cpp3/ex03/main.cpp
@@ -6,7 +6,7 @@ int	main()
 	std::cout << "Instanciating DiamondTrap.." << std::endl;
 	DiamondTrap dt666("dt666");
 	dt666.attack("dummy");
-	dt666.takeDamage(6);
+	dt666.takeDamage(10);
 	dt666.beRepaired(6);
 	dt666.guardGate();
 	dt666.highFivesGuys();
